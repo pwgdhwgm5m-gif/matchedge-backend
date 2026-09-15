77a84f72cee579a8c6ab744bdc036ba31a7248dc
pwgdhwgm5m-gif <pwgdhwgm5m@privaterelay.appleid.com>
2026-09-15 17:01:55 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -40,6 +40,16 @@ app.use('/api/analysis', analysisRoute);
 app.use('/api/live', liveRoute);
 app.use('/api/odds-history', oddsHistoryRoute);
 app.use('/api/results', resultsRoute);
+const tffScraper = require('./services/tffScraper');
+
+app.get('/test-tff-standings', async (req, res) => {
+  try {
+    const data = await tffScraper.getStandings();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message, stack: err.stack });
+  }
+});
 
 app.use((err, req, res, next) => {
   console.error('[server] Beklenmeyen hata:', err);
