65b908b86d1c5646852466baebe9073e26b1aaa1
pwgdhwgm5m-gif <pwgdhwgm5m@privaterelay.appleid.com>
2026-09-19 12:06:11 +0300

    Pass TSDB league ID into realtime analysis engine

--- src/routes/analysis.js
+++ src/routes/analysis.js
@@ -42,7 +42,7 @@ router.get('/:fixtureId', async (req, res) => {
 
   try {
     const analysisPromise = computeFullAnalysis({
-      fixtureId, home, away, homeTeamName, awayTeamName, league, leagueName, season, sportKey,
+      fixtureId, home, away, homeTeamName, awayTeamName, league, tsdbLeagueId, leagueName, season, sportKey,
     });
     const timeoutPromise = new Promise((_, reject) => setTimeout(() => reject(new Error('analysis_timeout')), 15000));
     const result = await Promise.race([analysisPromise, timeoutPromise]);
