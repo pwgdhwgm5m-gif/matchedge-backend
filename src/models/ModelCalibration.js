fdc05dbbaa3c6614f4fd0c7ce2ed757fdfc540fd
pwgdhwgm5m-gif <pwgdhwgm5m@privaterelay.appleid.com>
2026-09-22 14:53:53 +0300

    Version calibration records and validation metrics

--- src/models/ModelCalibration.js
+++ src/models/ModelCalibration.js
@@ -4,11 +4,14 @@ const schema = new mongoose.Schema({
   league: { type: String, required: true },
   market: { type: String, required: true },
   logitOffset: { type: Number, default: 0 },
+  calibrationVersion: { type: String, default: 'cal-v1' },
   active: { type: Boolean, default: false },
   trainCount: Number,
   validationCount: Number,
   baselineBrier: Number,
   adjustedBrier: Number,
+  baselineLogLoss: Number,
+  adjustedLogLoss: Number,
   trainedAt: Date,
 }, { timestamps: true });
 module.exports = mongoose.model('ModelCalibration', schema);
